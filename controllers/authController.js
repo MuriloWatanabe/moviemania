f5fcd4bede8444cd5213435df5e7ccc9ae6ced57
MuriloWatanabe <muriloenzowatanabe14@gmail.com>
2025-05-11 02:17:37 -0300

    Inicio da tela de registro

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,12 @@
+// controllers/authController.js
 const db = require('../models/db');
 
+// Exibe o formulário de login
 exports.exibirLogin = (req, res) => {
   res.render('login');
 };
 
+// Realiza o login do usuário
 exports.loginUsuario = async (req, res) => {
   const { email, senha } = req.body;
   const [usuario] = await db.query('SELECT * FROM usuarios WHERE email = ? AND senha = ?', [email, senha]);
@@ -12,5 +15,23 @@ exports.loginUsuario = async (req, res) => {
     res.send(`Bem-vindo(a), ${usuario[0].nome}!`);
   } else {
     res.send('Email ou senha incorretos!');
+  }
+};
+
+// Exibe o formulário de registro
+exports.viewRegister = (req, res) => {
+  res.render('register');
+};
+
+// Registra um novo usuário
+exports.userRegister = async (req, res) => {
+  const { email, senha } = req.body;
+
+  try {
+    await db.query('INSERT INTO usuarios (email, senha) VALUES (?, ?)', [email, senha]);
+    res.send("Usuário registrado com sucesso");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Erro ao registrar usuário");
   }
 };
--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,12 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
+// Rota para exibir o login
 router.get('/login', authController.exibirLogin);
 router.post('/login', authController.loginUsuario);
+
+// Rotas para o registro
+router.get('/register', authController.viewRegister);
+router.post('/register', authController.userRegister);
 
 module.exports = router;
